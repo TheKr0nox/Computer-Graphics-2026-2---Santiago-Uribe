69121771437475a58f99a925de2592f9536cbf23
TheKr0nox <santiagouribeu88@gmail.com>
2026-09-10 15:30:07 -0500

    parcial update

--- src/js/logicParcial.js
+++ src/js/logicParcial.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { metalness, roughness } from 'three/src/nodes/core/PropertyNode.js';
 
 // 1. ESCENA, CÁMARA Y RENDER
 const scene = new THREE.Scene();
@@ -39,11 +38,12 @@ const shapeData = [
 
     {
         name: 'Pilar 1',
-        geometry: new THREE.CylinderGeometry(10, 10, 10),
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 11.5),
         color: 0x965300,
-        posX: -2,
-        posY: 0,
+        posX: -2.5,
+        posY: 5,
         posZ: 0,
+        rotZ: -0.50,
         roughness: 0.9,
         metalness: 0
 
@@ -51,49 +51,109 @@ const shapeData = [
 
     {
         name: 'Pilar 2',
-        geometry: new THREE.CylinderGeometry(10, 10, 10),
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 11.5),
         color: 0x965300,
-        posX: 2,
-        posY: 0,
+        posX: 2.5,
+        posY: 5,
         posZ: 0,
+        rotZ: 0.50,
+        roughness: 0.9,
+        metalness: 0
+    },
+
+    {
+        name: 'Pilar 3',
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 11.5),
+        color: 0x965300,
+        posX: -2.5,
+        posY: 5,
+        posZ: 2,
+        rotX: 0.25,
+        rotZ: -0.35,
+        roughness: 0.9,
+        metalness: 0
+
+    },
+
+    {
+        name: 'Pilar 4',
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 11.5),
+        color: 0x965300,
+        posX: 2.5,
+        posY: 5,
+        posZ: -2,
+        rotX: -0.15,
+        rotZ: 0.35,
         roughness: 0.9,
         metalness: 0
     },
 
     {
         name: 'Rueda',
-        geometry: new THREE.TorusGeometry(20, 20, 20),
+        geometry: new THREE.TorusGeometry(6, 0.2, 15, 100),
         color: 0x666666,
         posX: 0,
+        posY: 10,
+        posZ: 1,
+        rotz: 1,
+        roughness: 0.1,
+        metalness: 0.5
+    },
+
+    {
+        name: 'Rueda2',
+        geometry: new THREE.TorusGeometry(6, 0.2, 15, 100),
+        color: 0x666666,
+        posX: 0,
+        posY: 10,
+        posZ: -1,
+        rotz: -1,
+        roughness: 0.1,
+        metalness: 0.5
+    },
+
+    {
+        name: 'medioRueda',
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 1.5),
+        color: 0x666666,
+        posX: 0,
+        posY: 10,
+        posZ: 1.2,
+        rotX: 4.7,
         roughness: 0.1,
         metalness: 0.1
     },
 
     {
-        name: 'Barra 1',
-        geometry: new THREE.CapsuleGeometry(20, 20, 20),
-        color: 153 - 153 - 153,
+        name: 'medioRueda2',
+        geometry: new THREE.CylinderGeometry(0.5, 0.5, 1.5),
+        color: 0x666666,
         posX: 0,
+        posY: 10,
+        posZ: -1.2,
+        rotX: 4.7,
         roughness: 0.1,
         metalness: 0.1
     },
 
     {
-        name: 'Barra 2',
-        geometry: new THREE.CapsuleGeometry(20, 20, 20),
-        color: 153 - 153 - 153,
+        name: 'EsferamedioRueda ',
+        geometry: new THREE.SphereGeometry(1, 22, 22),
+        color: 0xffff00,
         posX: 0,
+        posY: 10.2,
+        posZ: 0,
+        rotX: 4.7,
         roughness: 0.1,
         metalness: 0.1
     },
-
 ];
 
 for (let i = 1; i <= 8; i++) {
     shapeData.push({
         name: 'Cabinas',
         geometry: new THREE.BoxGeometry(2, 2, 2),
-        color: 0 - 0 - 255,
+        color: 0x0000ff,
         posX: 0,
         posY: 0,
         posZ: 0,
@@ -106,7 +166,7 @@ for (let i = 1; i <= 8; i++) {
     shapeData.push({
         name: 'Techos',
         geometry: new THREE.ConeGeometry(2, 2, 2),
-        color: 255 - 102 - 102,
+        color: 0xff6666,
         posX: 0,
         posY: 0,
         posZ: 0,
@@ -114,9 +174,71 @@ for (let i = 1; i <= 8; i++) {
         metalness: 0.1
     });
 }
+
+for (let i = 1; i <= 8; i++) {
+    shapeData.push({
+        name: 'BarrasMedio',
+        geometry: new THREE.CylinderGeometry(2, 2, 2),
+        color: 0x666666,
+        posX: 0,
+        posY: 0,
+        posZ: 0,
+        roughness: 0.2,
+        metalness: 0.1
+    });
+    shapeData.push({
+        name: 'BarrasMedioAtras',
+        geometry: new THREE.CylinderGeometry(0.1, 0.1, 12),
+        color: 0x666666,
+        posX: 0,
+        posY: 10,             
+        posZ: -1,           
+        rotZ: i/8 * Math.PI,
+        roughness: 0.2,
+        metalness:0
+    });
+
+    shapeData.push({
+        name: 'BarrasMedioAdelante',
+        geometry: new THREE.CylinderGeometry(0.1, 0.1, 12),
+        color: 0x666666,
+        posX: 0,
+        posY: 10,             
+        posZ: 1,           
+        rotZ: i/8 * Math.PI,
+        roughness: 0.2,
+        metalness:0
+    });
+}
+
 // =========================================================
 // TODO: CONSTRUIR LA RUEDA DE LA FORTUNA
 // =========================================================
+
+shapeData.forEach((shape) => {
+    const material = new THREE.MeshStandardMaterial({
+        color: shape.color,
+        roughness: shape.roughness,
+        metalness: shape.metalness
+    });
+
+    const mesh = new THREE.Mesh(shape.geometry, material);
+
+    mesh.position.set(
+        shape.posX || 0,
+        shape.posY || 0,
+        shape.posZ || 0
+    );
+
+    mesh.rotation.x = shape.rotX || 0;
+    mesh.rotation.y = shape.rotX || 0;
+    mesh.rotation.z = shape.rotZ || 0;
+
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    scene.add(mesh);
+});
 
 // En esta seccion, debes crear la rueda de la fortuna utilizando geometrías y materiales de Three.js. 
 const numCabinas = 8;
